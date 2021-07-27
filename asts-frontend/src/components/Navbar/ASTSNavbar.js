08dfc8fa4f9936f113cb3946a29f9f6304f72cf6
DiverseFX <rogamesfx@gmail.com>
2021-07-27 15:17:05 -0400

    Search bar for redirecting to ticker pages

--- asts-frontend/src/components/Navbar/ASTSNavbar.js
+++ asts-frontend/src/components/Navbar/ASTSNavbar.js
@@ -7,6 +7,7 @@ import {
   FormControl,
   Button,
 } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 ///////////////////////////////////////////////////////////////////////////////////
 
@@ -17,9 +18,22 @@ import { AuthContext } from "../../contexts/auth-context";
 
 function ASTSNavbar() {
   const auth = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [fundsLoaded, setFundsLoaded] = useState(false);
   const [fundNav, setFundNav] = useState();
+
+  const searchSubmitHandler = (event) => {
+    console.log("submitted");
+    console.log(event.search);
+    window.location.href = `http://localhost:3000/history/${event.search}`;
+  };
+
+  ///////////////////////////////////////////////////////////////////////////////////
 
   useEffect(() => {
     const getFunds = async () => {
@@ -52,45 +66,57 @@ function ASTSNavbar() {
     getFunds();
   }, [sendRequest]);
 
+  ///////////////////////////////////////////////////////////////////////////////////
+
   return (
     <>
-    <div
-      style={{
-        position: "absolute",
-        display: "flex",
-        width: "100vw",
-        "z-index": "100",
-      }}
-    >
-      <Navbar
+      <div
         style={{
-          backgroundColor: "#8364FF",
-          paddingLeft: "20vw",
-          paddingRight: "20vw",
-          filter: "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.2))",
+          position: "absolute",
+          display: "flex",
           width: "100vw",
-          position: "fixed",
-          height: "60px"
+          "z-index": "100",
         }}
-        variant="dark"
       >
-        <Navbar.Brand href="/">ASTS</Navbar.Brand>
+        <Navbar
+          style={{
+            backgroundColor: "#8364FF",
+            paddingLeft: "20vw",
+            paddingRight: "20vw",
+            filter: "drop-shadow(0px 0px 5px rgba(0, 0, 0, 0.2))",
+            width: "100vw",
+            position: "fixed",
+            height: "60px",
+          }}
+          variant="dark"
+        >
+          <Navbar.Brand href="/">ASTS</Navbar.Brand>
 
-        <Navbar.Collapse id="navbar-fund-drop">
-          <Nav>
-            <NavDropdown id="nav-fund-drop" title="Funds" menuVariant="dark">
-              {fundsLoaded && !isLoading && fundNav}
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+          <Navbar.Collapse id="navbar-fund-drop">
+            <Nav>
+              <NavDropdown id="nav-fund-drop" title="Funds" menuVariant="dark">
+                {fundsLoaded && !isLoading && fundNav}
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
 
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-light">Search</Button>
-        </Form>
-      </Navbar>
-    </div>
-    <div style={{height: "60px"}}/>
+          <Form inline onSubmit={handleSubmit(searchSubmitHandler)}>
+            <Form.Group controlId="search">
+              <FormControl
+                type="input"
+                placeholder="Search up a ticker"
+                className="mr-sm-2"
+                {...register("search", { required: "Can't be empty." })}
+              />
+            </Form.Group>
+
+            <Button variant="outline-light" type="submit">
+              Search
+            </Button>
+          </Form>
+        </Navbar>
+      </div>
+      <div style={{ height: "60px" }} />
     </>
   );
 }
