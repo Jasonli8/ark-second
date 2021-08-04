f119ee93380079772edca249aa1ca46f7166ca6e
DiverseFX <rogamesfx@gmail.com>
2021-08-04 16:21:08 -0400

    Recontain footer to match viewport

--- asts-frontend/src/components/Footer/Footer.js
+++ asts-frontend/src/components/Footer/Footer.js
@@ -13,28 +13,31 @@ function Footer() {
   return (
     <footer style={{ justifyContent: "center" }}>
       <br />
-      <div className="text-light row">
-        <div className="col-sm-3" />
-        <div className="col-sm-6">
-          <p>
-            This is a third party app designed to track stocks held by ARK
-            Invest ETFs. Data displayed here is meant as a reference, but may
-            not be accurately representative of real data. All rights are
-            reserved to ARK Invest, and this app is for private use only.{" "}
-          </p>
-          <br />
-          <p>
-            <strong>Created by:</strong>
+      <div className="container">
+        <div className="text-light row">
+          <div className="col-sm-3" />
+          <div className="col-sm-6">
+            <p>
+              This is a third party app designed to track stocks held by ARK
+              Invest ETFs. Data displayed here is meant as a reference, but may
+              not be accurately representative of real data. All rights are
+              reserved to ARK Invest, and this app is for private use only.{" "}
+            </p>
             <br />
-            Jason Li
-            <br />
-            jason.ming.li.business@gmail.com
-            <br />
-            (647) 803-8628
-          </p>
+            <p>
+              <strong>Created by:</strong>
+              <br />
+              Jason Li
+              <br />
+              jason.ming.li.business@gmail.com
+              <br />
+              (647) 803-8628
+            </p>
+          </div>
+          <div className="col-sm-3" />
         </div>
-        <div className="col-sm-3" />
       </div>
+
       <br />
       <Button className="btn btn-secondary p-3" onClick={auth.logout}>
         Logout
